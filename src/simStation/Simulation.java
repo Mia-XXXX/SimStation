722d732865878a0df1af557c8459535365ab1370
Mia-XXXX <huitang.mia@gmail.com>
2020-04-06 23:24:58 -0700

    add timer in simulation class

--- src/simStation/Simulation.java
+++ src/simStation/Simulation.java
@@ -9,13 +9,33 @@
 
 package simStation;
 
-import java.util.ArrayList;
+import mvc.Model;
 
-public abstract class Simulation {
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public abstract class Simulation extends Model {
     private int clock;
+    private Timer timer;
     private ArrayList<Agent> agents;
 
+    private void startTimer() {
+        timer = new Timer();
+        timer.scheduleAtFixedRate(new ClockUpdater(), 1000, 1000);
+    }
 
+    private void stopTimer() {
+        timer.cancel();
+        timer.purge();
+    }
+
+    private class ClockUpdater extends TimerTask {
+        public void run() {
+            clock++;
+            //changed();
+        }
+    }
 
     public Simulation(){
         clock = 0;
@@ -30,5 +50,7 @@ public abstract class Simulation {
     public abstract void stop();
 
     public abstract void populate();
+
+    public abstract ArrayList<String> getStats();
 
 }
