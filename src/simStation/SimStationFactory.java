c727f596f3a502b2a39a678b4d242627a3ca759e
zychin2000 <zychin2000@gmail.com>
2020-04-12 10:59:24 +0800

    Completed SimStation Factory, Panel and View

---
+++ src/simStation/SimStationFactory.java
@@ -0,0 +1,46 @@
+package simStation;
+
+import mvc.AppFactory;
+import mvc.Command;
+import mvc.Model;
+
+public abstract class SimStationFactory implements AppFactory {
+
+    @Override
+    public String[] getEditCommands() {
+        return new String[]{"Start", "Suspend", "Resume", "Stop", "Stats"};
+    }
+
+    @Override
+    public Command makeEditCommand(Model model, String type) {
+        switch (type) {
+            case "Start":
+                return new StartCommand(model);
+            case "Suspend":
+                return new SuspendCommand(model);
+            case "Resume":
+                return new ResumeCommand(model);
+            case "Stop":
+                return new StopCommand(model);
+            case "Stats":
+                return new StatsCommand(model);
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public String getTitle() {
+        return "SimStation";
+    }
+
+    @Override
+    public String[] getHelp() {
+        return new String[0];
+    }
+
+    @Override
+    public String about() {
+        return "SimStation, by Hui Tang, Phuc Phan and Zheng-Yao Chin";
+    }
+}
---
+++ src/simStation/SimStationView.java
@@ -0,0 +1,10 @@
+package simStation;
+
+import mvc.Model;
+import mvc.View;
+
+public class SimStationView extends View {
+    public SimStationView(Simulation model) {
+        super(model);
+    }
+}
