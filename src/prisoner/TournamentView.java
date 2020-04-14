6c94b58386f99a4c56104334e60415e3179d9638
phanhongphuc <phanhongphuc@gmail.com>
2020-04-13 21:58:44 -0700

    change the way random move and add color for prisoners

--- src/prisoner/TournamentView.java
+++ src/prisoner/TournamentView.java
@@ -1,7 +1,7 @@
 /*
  * Edit history:
- *   Phuc Phan, 4/13: created
- *
+ *  Phuc Phan, 4/13: created
+ *	Phuc Phan, 4/13: add color for prisoners
  *
  */
 package prisoner;
@@ -19,7 +19,8 @@ public class TournamentView extends SimStationView {
     @Override
     protected void paintComponent(Graphics gc) {
         Color oldColor = gc.getColor();
-
+        
+        gc.setColor(Color.RED);
         for (Agent agent : getAgents()) {
             int r = 10;
             gc.fillOval(agent.getXc(), agent.getYc(), r, r);
